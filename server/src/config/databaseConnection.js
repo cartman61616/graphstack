69d7ee3e876d36366901f01e506768cf1b821fb7
Jonathan Davis <cartman61616@gmail.com>
2020-05-29 15:38:39 -0400

    initial commit

---
+++ server/src/config/databaseConnection.js
@@ -0,0 +1,27 @@
+const MongoClient = require('mongodb').MongoClient;
+
+let mongoDB;
+
+const setupDB = callback => {
+    const uri = '' // get from mongo site
+
+    MongoClient.connect(
+        uri,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        (err, client) => {
+            mongoDB = client.db('full-stack-server');
+
+            if (err) {
+                return callback(err);
+            } else {
+                return callback('DB OK');
+            }
+        }
+    );
+};
+
+const getDB = () => {
+    return mongoDB;
+};
+
+module.exports = { setupDB, getDB };
